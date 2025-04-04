b822affb581bb838c11e700edcc3444e0529ec91
Re <112631682+mcregan126@users.noreply.github.com>
2025-04-03 22:13:08 -0400

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys = ON;
-
 INSERT INTO users(username, fullname, email, filename, password)
 VALUES 
+
 ('awdeorio', 'Andrew DeOrio', 'awdeorio@umich.edu', 'e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg', 'sha512$34e94a05cdf247db92a84bc590950336$7eaca2b4169e042120f015666115856c717343f1c75d1c1bd1bf469bd1cd439eb152ccda6a0b8703706dfbcb861b3cef9208325c31f436e8edb9563f01176c48'),
 ('mcregan', 'Regan McDonald', 'mcregan@umich.edu', '505083b8b56c97429a728b68f31b0b2a089e5113.jpg', 'sha512$673d22398b0141c7929f987efee061e6$187dd68d62574a29b40513467cb5376849d6e7651dbd19850b853b912f44d940a42ef6bb96f4bafa82a6b40072ed980bfad377c65faa096281369210841f2b73'),
